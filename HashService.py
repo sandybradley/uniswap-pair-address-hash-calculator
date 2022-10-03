e62d3f4283ca086de76ff361251e939aa2ea35cb
sandybradley <sandy.bradley@gmail.com>
2022-10-03 19:50:02 +0200

    feat: sushiswap, traderjoe, quickswap and spookyswap config added

--- HashService.py
+++ HashService.py
@@ -12,7 +12,56 @@ class HashService:
             factory_address='0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
             init_code_hash='0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
         )
+    
+    @staticmethod
+    def for_sushiswap():
+        return HashService(
+            factory_address='0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',
+            init_code_hash='0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+        )
+        
+    @staticmethod
+    def for_sushiswap_polygon():
+        return HashService(
+            factory_address='0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+            init_code_hash='0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+        )
+        
+    @staticmethod
+    def for_sushiswap_fantom():
+        return HashService(
+            factory_address='0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+            init_code_hash='0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+        )
 
+    @staticmethod
+    def for_sushiswap_avalanche():
+        return HashService(
+            factory_address='0xc35DADB65012eC5796536bD9864eD8773aBc74C4',
+            init_code_hash='0xe18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303',
+        )
+    
+    @staticmethod
+    def for_quickswap():
+        return HashService(
+            factory_address='0x5757371414417b8C6CAad45bAeF941aBc7d3Ab32',
+            init_code_hash='0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f',
+        )
+        
+    @staticmethod
+    def for_spookyswap():
+        return HashService(
+            factory_address='0x152eE697f2E276fA89E96742e9bB9aB1F2E61bE3',
+            init_code_hash='0xcdf2deca40a0bd56de8e3ce5c7df6727e5b1bf2ac96f283fa9c4b3e6b42ea9d2',
+        )
+        
+    @staticmethod
+    def for_traderjoe():
+        return HashService(
+            factory_address='0x9Ad6C38BE94206cA50bb0d90783181662f0Cfa10',
+            init_code_hash='0x0bbca9af0511ad1a1da383135cf3a8d2ac620e549ef9f6ae3a4c33c2fed0af91',
+        )
+               
     @staticmethod
     def for_pancake_swap():
         return HashService(
